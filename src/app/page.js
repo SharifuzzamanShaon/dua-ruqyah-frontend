25259108f9bd071dffb29f8aa46c6648f2ecf74f
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-16 13:16:57 +0600

    grid rearraged

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,41 @@
+"use client"
 import Categories from "@/components/Categories";
+import DisplayItems from "@/components/DisplayItems";
 import MainMenuBar from "@/components/MainMenuBar";
+import Settings from "@/components/Settings";
+import Topbar from "@/components/Topbar";
+import { useState } from "react";
 
 export default function Home() {
+  const [duas, setDuas] = useState([])
+  const [navigate, setNavigate] = useState(0)
   return (
-    <div className="flex w-full h-screen">
-      <div className="grid grid-cols-12 gap-4 w-full">
-        <div className="col-span-12 sm:col-span-1 bg-gray-100 text-center">
-          <MainMenuBar />
+    <>
+      <Topbar />
+      <div className="flex w-full h-screen overflow-hidden">
+        <div className="grid grid-cols-12 w-full h-full">
+          {/* MainMenuBar */}
+          <div className="col-span-12 sm:col-span-1 bg-gray-100 flex justify-center">
+            <MainMenuBar />
+          </div>
+          
+          {/* Categories */}
+          <div className="col-span-12 sm:col-span-3 overflow-auto p-4 bg-white">
+            <h2 className="text-lg font-bold mb-2">DUA PAGE</h2>
+            <Categories duas={duas} setDuas={setDuas} setNavigate={setNavigate}/>
+          </div>
+          
+          {/* DisplayItems */}
+          <div className="col-span-12 sm:col-span-6 overflow-auto p-1 bg-white">
+            <DisplayItems duas={duas}/>
+          </div>
+          
+          {/* Settings */}
+          <div className="col-span-12 sm:col-span-2 bg-gray-100 px-2">
+            <Settings />
+          </div>
         </div>
-        <div className="col-span-12 sm:col-span-2 bg-gray-300 my-5">
-          DUA PAGE
-          <Categories></Categories>
-        </div>
-        <div className="col-span-12 sm:col-span-6 bg-gray-400">50%</div>
-        <div className="col-span-12 sm:col-span-2 bg-gray-600">20% fvef</div>
       </div>
-    </div>
+    </>
   );
 }
