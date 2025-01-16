f82a939e1ceac6192648f087a4a18113cd1aecb1
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-16 20:57:53 +0600

    font color

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,22 +4,22 @@ const Settings = () => {
   return (
     <>
       <div className="w-full mx-auto bg-gray-50 rounded-xl shadow-lg ">
-        <h1 className="text-center text-xl font-semibold mb-6">Settings</h1>
+        <h1 className="text-center text-md font-semibold mb-6 text-black">Settings</h1>
         <div className="space-y-4">
           {/* Language Settings */}
-          <div className="bg-white rounded-lg shadow-sm p-4 flex items-center">
+          <div className="bg-white text-black rounded-lg shadow-sm p-4 flex items-center">
             <div className="w-10 h-10 bg-green-100 flex items-center justify-center rounded-full">
               <i className="fas fa-language text-green-600"></i>
             </div>
             <div className="flex-1 ml-4">
-              <h2 className="text-sm font-semibold text-green-600">
+              <h2 className="text-sm font-semibold text-green-600 text-black">
                 Language Settings
               </h2>
               <div className="flex gap-2 mt-2">
-                <button className="px-4 py-2 bg-green-500 text-black rounded-md text-sm">
+                <button className="px-4 py-2 bg-green-500 text-black rounded-md text-sm text-black">
                   English
                 </button>
-                <button className="px-4 py-2 bg-white border border-gray-300 rounded-md text-sm">
+                <button className="px-4 py-2 bg-white border border-gray-300 rounded-md text-sm text-black">
                   বাংলা
                 </button>
               </div>
@@ -31,7 +31,7 @@ const Settings = () => {
             <div className="w-10 h-10 bg-gray-200 flex items-center justify-center rounded-full">
               <i className="fas fa-cog text-gray-500"></i>
             </div>
-            <h2 className="ml-4 text-sm font-medium">General Settings</h2>
+            <h2 className="ml-4 text-sm font-medium text-black">General Settings</h2>
           </div>
 
           {/* Font Settings */}
@@ -39,11 +39,11 @@ const Settings = () => {
             <div className="w-10 h-10 bg-gray-200 flex items-center justify-center rounded-full">
               <i className="fas fa-font text-gray-500"></i>
             </div>
-            <h2 className="ml-4 text-sm font-medium">Font Settings</h2>
+            <h2 className="ml-4 text-sm font-medium text-black">Font Settings</h2>
           </div>
 
           {/* Appearance Settings */}
-          <div className="bg-gray-100 rounded-lg shadow-sm p-4 flex items-center">
+          <div className="bg-gray-100 rounded-lg shadow-sm p-4 flex items-center text-black">
             <div className="w-10 h-10 bg-gray-200 flex items-center justify-center rounded-full">
               <i className="fas fa-palette text-black-500"></i>
             </div>
