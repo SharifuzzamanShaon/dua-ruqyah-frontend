8a3a62cc572c2a826df4cf400a77aee8df5b4a9e
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-16 13:17:26 +0600

    left menubar

--- src/components/MainMenuBar.jsx
+++ src/components/MainMenuBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { CiHome } from "react-icons/ci";
 import { TbCategoryPlus } from "react-icons/tb";
+import { IoBulbOutline } from "react-icons/io5";
+import { FiTag } from "react-icons/fi";
 
 const MainMenuBar = () => {
   return (
-    <div className="text-black flex flex-col items-center justify-center mt-5 space-y-4">
+    <div className="text-black flex flex-col pt-4 space-y-4">
       <CiHome className="text-4xl" />
       <TbCategoryPlus className="text-4xl" />
-      <TbCategoryPlus className="text-4xl" />
-      <TbCategoryPlus className="text-4xl" />
-      <TbCategoryPlus className="text-4xl" />
+      <IoBulbOutline className="text-4xl" />
+      <FiTag className="text-4xl" />
       <TbCategoryPlus className="text-4xl" />
       <TbCategoryPlus className="text-4xl" />
     </div>
