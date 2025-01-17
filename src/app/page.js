853183c7f5cbaa7137a8076b32341d6d0505313e
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-17 19:19:07 +0600

    change the gap

--- src/app/page.js
+++ src/app/page.js
@@ -19,25 +19,25 @@ export default function Page() {
 
       {/* Main Layout */}
       <div className="flex flex-grow w-full overflow-hidden">
-        <div className="grid grid-cols-12 w-full h-full">
+        <div className="grid grid-cols-12 w-full h-full gap-4 px-4">
           {/* MainMenuBar */}
-          <div className="col-span-12 sm:col-span-1 bg-gray-100 flex justify-center">
+          <div className="col-span-12 sm:col-span-2 lg:col-span-1 bg-gray-100 flex justify-center items-start p-2 sm:p-4">
             <MainMenuBar />
           </div>
 
           {/* Categories */}
-          <div className="col-span-12 sm:col-span-3 overflow-hidden p-4 bg-white">
-            <h2 className="text-lg font-bold ">DUA PAGE</h2>
+          <div className="col-span-12 sm:col-span-4 lg:col-span-3 bg-white p-2 sm:p-4">
+            <h2 className="text-lg font-bold mb-4">DUA PAGE</h2>
             <Categories duas={duas} setDuas={setDuas} setNavigate={setNavigate} />
           </div>
 
           {/* DisplayItems */}
-          <div className="col-span-12 sm:col-span-6 overflow-auto p-1 bg-white">
+          <div className="col-span-12 sm:col-span-8 lg:col-span-6 bg-white p-2 sm:p-4 overflow-auto">
             <DisplayItems duas={duas} navigate={navigate} />
           </div>
 
           {/* Settings */}
-          <div className="col-span-12 sm:col-span-2 bg-gray-100 px-2">
+          <div className="col-span-12 sm:col-span-3 lg:col-span-2 bg-gray-100 px-2 sm:px-4 py-4">
             <Settings />
           </div>
         </div>
